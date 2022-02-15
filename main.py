4653bcdcd2550df7748f082a021423aa961c23a9
sorani-dev <sorani.dev@outlook.com>
2022-02-16 00:42:28 +0100

    feat: add paddle movement

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Sequence
 import pygame
 from pygame.surface import Surface
 
@@ -26,7 +26,8 @@ PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
 class Paddle():
     """Paddle shape
     """
-    COLOR = WHITE  # paddle color
+    COLOR = WHITE  # Paddle color
+    VELOCITY = 4  # Paddle velocity
 
     def __init__(self, x: int, y: int, width: int, height: int) -> None:
         """
@@ -42,8 +43,25 @@ class Paddle():
         self.width = width
 
     def draw(self, win: pygame.surface.Surface) -> None:
+        """Draw paddle on screen
+
+        Args:
+            win (pygame.surface.Surface): Pygame window
+        """
         pygame.draw.rect(win, self.COLOR,
                          (self.x, self.y, self.width, self.height))
+
+    def move(self, up: bool = True) -> None:
+        """Move paddle up or down the screen depending on the selected key
+
+        Args:
+            up (bool, optional): Which direction is the paddle supposed to move (Up or Down). Defaults to True.
+        """
+        # Move up or down
+        if up:
+            self.y -= self.VELOCITY
+        else:
+            self.y += self.VELOCITY
 
 
 def draw(win: pygame.surface.Surface, paddles: List[Paddle]):
@@ -59,12 +77,33 @@ def draw(win: pygame.surface.Surface, paddles: List[Paddle]):
     pygame.display.update()
 
 
+def handle_paddle_movement(keys: Sequence[bool], left_paddle: Paddle, right_paddle: Paddle) -> None:
+    """Change the paddle movement on which key is pressed
+
+    Args:
+        keys (Sequence[bool]): Keys pressed
+        left_paddle (Paddle) 
+        right_paddle (Paddle)
+    """
+    # Left paddle movement
+    if keys[pygame.K_w] and left_paddle.y - left_paddle.VELOCITY >= 0:
+        left_paddle.move(up=True)
+    if keys[pygame.K_s] and left_paddle.y + left_paddle.VELOCITY + left_paddle.height <= HEIGHT:
+        left_paddle.move(up=False)
+
+    # Right paddle movement
+    if keys[pygame.K_UP] and right_paddle.y - right_paddle.VELOCITY >= 0:
+        right_paddle.move(up=True)
+    if keys[pygame.K_DOWN] and right_paddle.y + right_paddle.VELOCITY + right_paddle.height <= HEIGHT:
+        right_paddle.move(up=False)
+
+
 def main():
     """"Program"""
     # Is the game running?
     run = True
 
-    # make game run on same framerame on every computer
+    # Make game run on same framerame on every computer
     clock = pygame.time.Clock()
 
     # Paddles
@@ -82,9 +121,14 @@ def main():
 
         # Check events
         for event in pygame.event.get():
+            # Quit game
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        # Get all keyboard keys pressed
+        keys = pygame.key.get_pressed()
+        handle_paddle_movement(keys, left_paddle, right_paddle)
 
     pygame.quit()
 
